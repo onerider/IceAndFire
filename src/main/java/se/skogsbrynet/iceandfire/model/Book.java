1bf7a2c5fcaad2f033bf36a71b62abca6b3a1b59
Tommy <tommy.enryd@gmail.com>
2017-10-08 20:36:49 +0200

    new entities

---
+++ src/main/java/se/skogsbrynet/iceandfire/model/Book.java
@@ -0,0 +1,12 @@
+package se.skogsbrynet.iceandfire.model;
+
+import se.skogsbrynet.iceandfire.service.UrlService;
+
+/**
+ * Entity Book, mapping to
+ * the IceAndFire JSON-object Book
+ */
+public class Book extends Entity {
+
+
+}
---
+++ src/main/java/se/skogsbrynet/iceandfire/model/House.java
@@ -0,0 +1,12 @@
+package se.skogsbrynet.iceandfire.model;
+
+import se.skogsbrynet.iceandfire.service.UrlService;
+
+/**
+ * Entity House, mapping to
+ * the IceAndFire JSON-object House
+ */
+public class House extends Entity {
+
+
+}
