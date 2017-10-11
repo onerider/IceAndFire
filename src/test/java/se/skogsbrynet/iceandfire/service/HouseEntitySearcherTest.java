9e6a8b20ec3aa90f9b2d4b6009fa4a5ff3cc196b
Tommy <tommy.enryd@gmail.com>
2017-10-11 23:24:32 +0200

    code cleanup

--- src/test/java/se/skogsbrynet/iceandfire/service/HouseEntitySearcherTest.java
+++ src/test/java/se/skogsbrynet/iceandfire/service/HouseEntitySearcherTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+@SuppressWarnings("SpellCheckingInspection")
 public class HouseEntitySearcherTest {
     @Test
     public void search() throws Exception {
